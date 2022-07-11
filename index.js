3ed7a0fdc721de23940efadc109d504749389a6e
wmauriciob <mauricio.buitrago.p@gmail.com>
2022-07-11 02:32:16 -0500

    2 files modified

--- index.js
+++ index.js
@@ -11,3 +11,25 @@ var visualization = d3plus.viz()
 .x("year")
 .y("value")
 .draw()
+
+d3.json("https://raw.githubusercontent.com/raul27868/07MBIG-Visualizacion-Actividades-Guiadas/master/columnas.json", function(data) {
+
+  var visualization = d3plus.viz()
+  
+  .container("#viz2")
+  
+  .data(data)
+  
+  .type('bar')
+  
+  .id('name')
+  
+  .x('year')
+  
+  .y('value')
+  
+  .axes({ ticks: 'false' })
+  
+  .draw();
+  
+  });
