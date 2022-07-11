e5b2874439fef965b619b9a5a970aa193cdc58ec
wmauriciob <mauricio.buitrago.p@gmail.com>
2022-07-11 02:13:57 -0500

    2 files modified

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 // Import stylesheets
 import './style.css';
 
+//Gráfico de columnas. Mi 1o gráfico
+var visualization = d3plus.viz()
+.container("#viz1")
+.data([{"year": 1991, "name":"alpha", "value": 15},
+{"year": 1992, "name":"alpha", "value": 20},])
+.type("bar")
+.id("name")
+.x("year")
+.y("value")
+.draw()
